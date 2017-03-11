2f9617c532d2d62c6266294609020f81384ed427
k911 <kondzio9114@gmail.com>
2017-03-12 00:12:30 +0100

    update application structure

--- application/core/Controller/Base.php
+++ application/core/Controller/Base.php
@@ -5,6 +5,7 @@ use Gideon\Controller;
 use Gideon\Handler\Config;
 use Gideon\Handler\Locale;
 use Gideon\Http\Request;
+use Gideon\Database\Connection;
 
 abstract class Base implements Controller
 {
@@ -13,8 +14,8 @@ abstract class Base implements Controller
      * @var Gideon\Handler\Locale           $locale
      * @var Gideon\Http\Request             $request
      * @var Gideon\Http\Request\Params      $params
+     * @var Gideon\Database\Connection      $connection;
      *
-     * @todo @var Gideon\DBConnection       $db;
      * @todo @var Gideon\Http\Cookie        $cookie
      * @todo @var Gideon\Http\CSRF          $csrf
      * @todo @var Gideon\Model              $model
@@ -24,11 +25,12 @@ abstract class Base implements Controller
     protected $request;
     protected $params;
 
-    public function __construct(Config $config, Locale $locale, Request $request)
+    public function init(Config $config, Locale $locale, Request $request, Connection $connection)
     {
         $this->config = $config;
         $this->locale = $locale;
         $this->request = $request;
+        $this->connection = $connection;
         $this->params = new Request\Params($request->method());
     }
     
